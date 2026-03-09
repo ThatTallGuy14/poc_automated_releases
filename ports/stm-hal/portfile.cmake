7b66072e44bb957e91d253b6ad12e80a710c316e
Rees Varney <rees.varney@apollo-fire.com>
2026-03-09 10:11:45 +0000

    feat!: multiple skus for the HAL

--- ports/stm-hal/portfile.cmake
+++ ports/stm-hal/portfile.cmake
@@ -4,6 +4,17 @@ set(BUILD_DIR "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel")
 
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
+# Select target SKU from vcpkg features.
+set(STM_HAL_SKU "STM32U031")
+if("stm32u031" IN_LIST FEATURES AND "stm32u083" IN_LIST FEATURES)
+  message(
+    FATAL_ERROR
+      "Features 'stm32u031' and 'stm32u083' are mutually exclusive for port '${PORT}'."
+  )
+elseif("stm32u083" IN_LIST FEATURES)
+  set(STM_HAL_SKU "STM32U083")
+endif()
+
 # Determine generator based on target
 if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Generic")
   set(GENERATOR_ARG -G "Ninja")
@@ -22,25 +33,17 @@ execute_process(
     "${CMAKE_COMMAND}" ${GENERATOR_ARG} -S "${SOURCE_PATH}" -B "${BUILD_DIR}"
     -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${CURRENT_PACKAGES_DIR}
     -DCMAKE_PREFIX_PATH=${CURRENT_INSTALLED_DIR} ${TOOLCHAIN_ARG}
-    -DCMAKE_CXX_SCAN_FOR_MODULES=OFF ${VCPKG_CMAKE_CONFIGURE_OPTIONS}
+    -DCMAKE_CXX_SCAN_FOR_MODULES=OFF -DSTM_HAL_SKU=${STM_HAL_SKU}
+    ${VCPKG_CMAKE_CONFIGURE_OPTIONS}
   RESULT_VARIABLE result)
-if(result)
-  message(FATAL_ERROR "CMake configure failed")
-endif()
 
 # Build
 execute_process(COMMAND "${CMAKE_COMMAND}" --build "${BUILD_DIR}" --config
                         Release RESULT_VARIABLE result)
-if(result)
-  message(FATAL_ERROR "CMake build failed")
-endif()
 
 # Install
 execute_process(COMMAND "${CMAKE_COMMAND}" --install "${BUILD_DIR}" --config
                         Release RESULT_VARIABLE result)
-if(result)
-  message(FATAL_ERROR "CMake install failed")
-endif()
 
 # Remove debug folder
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
